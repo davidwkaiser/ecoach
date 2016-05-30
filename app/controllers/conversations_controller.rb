f6238a29191164a5731e4eaba2fb7312da27d181
davidwkaiser <davidwkaiser@yahoo.com>
2016-05-30 10:56:27 -0500

    add app files

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,44 @@
+class ConversationsController < ApplicationController
+  def index
+   @conversation = Conversation.new
+  end
+
+  def new
+   @conversation = Conversation.new
+  end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+    if @conversation.save
+      flash[:notice] = ["Let's get started!"]
+      @conversation.turns << Turn.new(utterance: @conversation.first_question)
+      redirect_to conversation_path(@conversation.id)
+    else
+      flash[:errors] = @conversation.errors.full_messages.uniq
+      redirect_to '/'
+    end
+  end
+
+  def show
+    @conversation = Conversation.find(params[:id])
+  end
+
+  def update
+    @conversation = Conversation.find(params[:id])
+    if @conversation.save
+      flash[:notice] = ["Your conversation will be emailed to you shortly"]
+      ConversationMailer.send_conversation(@conversation)
+      redirect_to '/'
+    else
+      flash.errors.full_messages.uniq
+      redirect_to conversation_path(@conversation.id)
+    end
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:email)
+  end
+
+end
---
+++ app/controllers/turns_controller.rb
@@ -0,0 +1,22 @@
+class TurnsController < ApplicationController
+  def new
+    @turn = Turn.new
+  end
+
+  def create
+    @conversation = Conversation.find(params[:conversation_id])
+    @turn = @conversation.turns.create(turn_params)
+    @conversation.turns << Turn.new(utterance: Conversation.question)
+    redirect_to conversation_path(@conversation)
+  end
+
+  def get_question
+    Conversation.question
+  end
+
+  private
+
+  def turn_params
+    params.require(:turn).permit(:utterance)
+  end
+end
