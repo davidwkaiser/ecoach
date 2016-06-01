1b95cf8a462c26ad08544b533bed8fa9a66d3d69
davidwkaiser <davidwkaiser@yahoo.com>
2016-05-31 21:50:06 -0500

    get working, nominally, without ajax

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160601024016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "conversations", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "turns", force: :cascade do |t|
+    t.string   "utterance"
+    t.integer  "conversation_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "turns", ["conversation_id"], name: "index_turns_on_conversation_id", using: :btree
+
+  add_foreign_key "turns", "conversations"
 end
